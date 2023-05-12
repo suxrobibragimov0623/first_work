3fd9767efb2e0cc6d9494b7825a7a8c3d4024390
SuxrobIbragimov <suxrobibragimov0623@gmail.com>
2023-05-12 15:09:11 +0500

    My first commit

---
+++ SwitchMatch.php
@@ -0,0 +1,275 @@
+<?php
+//for takrorlash operatorlari
+
+//2-savol
+//for ($x = 2; $x <= 5; $x++) {
+//    echo $x;
+//}
+
+//funksiya
+
+//1-savol
+//function sign( $a)
+//{
+//    if ($a < 0 ) {
+//        echo -1;
+//    } elseif ($a==0){
+//        echo 0;
+//    }else{
+//        echo 1;
+//    }
+//}
+//
+//sign(3);
+
+
+// butun sonlar ustida amallar
+//1-savol
+
+//$a=1025;
+//$b=100;
+//$c=.25;
+//if($a<0){
+//    echo$a;
+//}elseif ($a=$a/$b){
+//    echo $a-$c;
+//} else{
+//    echo "false";
+//}
+
+//2-savol
+// $b= 5000;
+// $a = 1000;
+
+// if($b==0){
+//     echo "false";
+// }elseif ($b=$b/$a){
+//     echo $b;
+// }
+//3-savol
+
+//$a= 2048;
+//$b = 1000;
+//if($a==0){
+//    echo "false";
+//}elseif ($a=$a/$b){
+//    echo $a-.048;
+//}else{
+//    echo "false";
+//}
+
+//4-savol
+
+//$b= 24;
+//$a = 3;
+//if($b==0){
+//    echo $b;
+//}
+//elseif ($b=$b/$a){
+//    echo $b;
+//}
+//6-savol
+// $a=65;
+// $b=10;
+// $c=.5;
+// if($a>0){
+//     echo $a/$b-$c." ";
+//     echo $a%$b;
+// }
+
+
+// shart operatorlari
+
+// 1-savol
+//  $num = -1;
+
+//  if( $num > 0){
+//     echo ++$num;
+//  }
+// else {
+//    echo $num;
+// }
+
+
+// 2-savol
+// $num = 4;
+
+//  if( $num < 0){
+//     echo ++$num;
+//  }
+// else {
+//    echo $num - 2;
+// }
+
+
+// 3-savol
+// $num = 5;
+// if($num <0){
+//     echo $num-2;
+// }
+// elseif($num==0){
+//     echo $num=10;
+// }
+// else{
+//     echo $num;
+// }
+// 4,5-savol
+// $a = 4;
+// $b = -5;
+// $c = 6;
+// $count = 0;
+// $minuscount = 0;
+// if($a>0){
+//     ++$count;
+// }
+// else{
+//     echo ++$minuscount;
+// }
+// if($b>0){
+//     ++$count;
+// }
+// else{
+//     echo ++$minuscount;
+// }
+// if($c>0){
+//     ++$count;
+// }
+// else{
+//     echo ++$minuscount;
+// }
+// echo "\n";
+// echo $count;
+
+// 6-savol
+
+
+//$num = -2;
+//$num2 = 0;
+//
+//if($num>0){
+//    echo $num = $num2;
+//}
+//else{
+//    echo $num2;
+//}
+//7-savol
+
+// $a = -6;
+// $b = 3;
+
+// if($a < $b){
+//     echo $a != $b;
+// }
+
+// 10-savol
+//  $a = 12;
+//  $b = 13;
+//  if($a != $b){
+//  echo $a + $b;
+//  }
+// elseif($a = $b){
+//     echo $a=0 + $b=0;
+//  }
+//11-savol
+
+//  $a = 45;
+//  $b = 13;
+//  if($a == $b){
+//  echo $a=0 + $b=0;
+//  }
+// elseif($a !== $b){
+//     echo $a + !$b;
+//  }
+
+//12-savol
+
+// $a = 4;
+// $b = 2;
+// $c = 6;
+//
+//if($a>$b and $b<$c){
+//    echo $b;
+//}else{
+//    echo"false";
+//}
+
+//function abc($a, $b, $c)
+//{
+//    if ($a >= $b and $b >=$c) {
+//        echo $c;
+//    } elseif($a >= $b and $b<=$c) {
+//        echo $b;
+//    }if($a <= $b and $b <=$c){
+//        echo $a;
+//    }else{
+//        echo $a<=$b and $b>=$c;
+//    }
+//}
+
+//abc(6, 7, 8);
+
+// boolen
+//1-savol
+
+//$a=2;
+//if($a>0){
+//    echo "true";
+//}elseif ($a<0){
+//    echo "false";
+//}
+//2-savol
+
+//$a=4;
+//
+//if($a>0){
+//    echo $a%2;
+//}else{
+//    echo "funksiya to`g`ri emas";
+//}
+
+//3-savol
+//$a=9;
+//
+//if($a>0){
+//    echo $a%2;
+//}else{
+//    echo "funksiya to`g`ri emas";
+//}
+
+//4-savol
+
+//$a=1;
+//$b=2;
+//if($a>2 and $b<3){
+//    echo "true";
+//}else{
+//    echo"false";
+//}
+
+//5-savol
+//$a=1;
+//$b=-3;
+
+//if($a>0 and $b<-2){
+//    echo "true";
+//}else{
+//    echo"false";
+//}
+
+//6,7-savol
+
+//$a=3;
+//$b=2;
+//$c=1;
+//if($a<=$b and $b<=$c){
+//    echo "true";
+//}else{
+//    echo"false";
+//}
+
+
+
+
+
+
+
